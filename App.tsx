7c37d37b065f07860420856bedd49b5c12121e60
seungyeop Kim <seungyeop2002@gmail.com>
2021-03-01 18:01:34 +0900

    Add Weather page

--- App.tsx
+++ App.tsx
@@ -4,18 +4,28 @@ import React, { useEffect, useState } from "react";
 import * as Location from "expo-location";
 
 import Loading from "./Loading";
+import Weather, { WeatherCondition } from "./Weather";
 
 const API_KEY = "d172bb83f8e98bfb107a16a9714e54c8";
 
 const App = () => {
   const [isLoading, setIsLoading] = useState(true);
+  const [temp, setTemp] = useState(0);
+  const [condition, setCondition] = useState<WeatherCondition>("Clear");
 
   useEffect(() => {
     const getWeahter = async (latitude: number, longitude: number) => {
-      const { data } = await axios.get(
-        `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}`
+      const {
+        data: {
+          main: { temp },
+          weather,
+        },
+      } = await axios.get(
+        `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&units=metric`
       );
-      console.log(data);
+      setIsLoading(false);
+      setTemp(temp);
+      setCondition(weather[0].main);
     };
     const getLocation = async () => {
       try {
@@ -24,7 +34,6 @@ const App = () => {
           coords: { latitude, longitude },
         } = await Location.getCurrentPositionAsync();
         getWeahter(latitude, longitude);
-        setIsLoading(false);
       } catch (error) {
         Alert.alert("Can't find you.", "So sad");
       }
@@ -32,7 +41,11 @@ const App = () => {
     getLocation();
   }, [isLoading]);
 
-  return isLoading ? <Loading /> : null;
+  return isLoading ? (
+    <Loading />
+  ) : (
+    <Weather temp={Math.round(temp)} condition={condition} />
+  );
 };
 
 export default App;
